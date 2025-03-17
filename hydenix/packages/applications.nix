3e9bf2d38f903e7103852db4b2fa211dc585eac4
Chun Yu <lamchunyu00@gmail.com>
2025-03-17 15:32:05 +0800

    fix: ark to kdePackages.ark

--- hydenix/packages/applications.nix
+++ hydenix/packages/applications.nix
@@ -6,7 +6,7 @@
     firefox # browser
     kitty # terminal
     kdePackages.dolphin # kde file manager
-    ark # kde file archiver
+    kdePackages.ark # kde file archiver
     vim # terminal text editor
     vscode # ide text editor
     discord # chat client
